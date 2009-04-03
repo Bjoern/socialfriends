b93546f0c018dbb6a5448d5fc58cd6dd5c4a44d5
Björn Günzel <bjoern@Maggie.(none)>
2009-04-03 21:18:36 +0200

    app helper, prefilled search field, some layout tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+    #assume text is already escaped with h() or something, just take care of links
+    def twitter_status(user, msg)
+        "#{link_to_twitter_profile user} #{msg}"
+    end
+
+    def link_to_twitter_profile(user)
+        "<a href=\"http://twitter.com/#{h(user)}\">#{h(user)}</a>"
+    end
 end
